ba3ff5488ed416375cb157be7998e31e24a4d923
Matt Scott <scott.md1617@gmail.com>
2024-02-28 15:51:02 +0000

    finished

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle; //holds a circle
+        var circles = []; //stores all circles
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2); //draws the circle in the vanvas
+            physikz.addRandomVelocity(circle, canvas); //gives the circle its velocity
+            view.addChild(circle); //allows the circle to appear on the screen
+            circles.push(circle); //adds the circle to the array "circles"
+        }      
 
-        // TODO 3 / 7 : Call the drawCircle() function 
-
+        // TODO 3 / 7 : Call the drawCircle() function
+        for (var loops = 0; loops <= 100; loops++){ //creates a loop taht repeats 100 times
+            drawCircle(); //draws a circle 100 times
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,13 +47,14 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
 
-            // TODO 9 : Iterate over the array
-           
+            // TODO 8 / 9 : Iterate over the array
+           for (var i = 0; i < circles.length; i++){ //creates a loop that repeats as many times as the array "circles" has values, in this case 100.
+             physikz.updatePosition(circles[i]) //updates the value of the position of the circle
+             game.checkCirclePosition(circles[i]) //finds the circle's position
+           }
             
         }
     
@@ -56,14 +65,27 @@ var init = function (window) {
         */
         game.checkCirclePosition = function(circle) {
 
+            var rightEdge = circle.x + circle.radius; //creates a variable for the right edge of the circle
+            var leftEdge = circle.x - circle.radius; //creates a variable for the left edge of the circle
+            var topEdge = circle.y - circle.radius; //creates a variable for the top edge of the circle
+            var bottomEdge = circle.y + circle.radius; //creates a variable for the bottom edge of the circle
+
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
+            if ( leftEdge > canvas.width ) {
+                circle.x = 0 - circle.radius; //sets the x position of the circle to slightly below zero so the circle smoothly transitions from right to left
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
-
+            if ( topEdge > canvas.height ) {
+                circle.y = 0 - circle.radius; //sets the y position of the circle to slightly below zero so the circle smoothly transitions from the bottom to the top
+            }
+            if ( rightEdge < 0 ) {
+                circle.x = canvas.width + circle.radius; //sets the x position of the circle to slightly to the right of the convas so the circle smoothly transitions from left to right
+            }
+            if ( bottomEdge < 0) {
+                circle.y = canvas.height + circle.radius; //sets the y position of the circle to slightly below the canvas so the circle smoothly transitions from the top to the bottom
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
